9a86c97634821aee296c41e81a11e5de960d22ac
MaxYDV <y.d.v.m.2010@gmail.com>
2023-07-05 11:32:51 +0300

    The first task is completed.

---
+++ main.py
@@ -0,0 +1,43 @@
+# У всіх завданнях використовувати обробку винятків
+
+# 1. Користувач вводить із клавіатури номер дня тижня (1-7).
+# Необхідно вивести на екран назви дня тижня.
+# Наприклад, якщо 1, на екрані напис понеділок, 2 — вівторок тощо.
+
+##################################################################
+try:
+    dayNambers = int(input("Enter day numbers of the week (1-7): "))
+    match dayNambers:
+        case 1:
+            print(f"{dayNambers} day of the week is Monday.")
+        case 2:
+            print(f"{dayNambers} day of the week is Tuesday.")
+        case 3:
+            print(f"{dayNambers} day of the week is Wednesday.")
+        case 4:
+            print(f"{dayNambers} day of the week is Thursday.")
+        case 5:
+            print(f"{dayNambers} day of the week is Friday.")
+        case 6:
+            print(f"{dayNambers} day of the week is Saturday.")
+        case 7:
+            print(f"{dayNambers} day of the week is Sunday.")
+        case _:
+            print("Enter a number in the range (1-7)!")
+except ValueError as error:
+   print("Enter only integer!")
+
+##################################################################
+# 2. Користувач вводить два числа.
+# Визначити, чи рівні ці числа, і, якщо ні, вивести їх на екран у порядку зростання
+
+##################################################################
+
+##################################################################
+
+# 3. Користувач вводить два числа та матем дію: + - * або /.
+# Залежно від введеної матем дії вивести результат.
+
+##################################################################
+
+##################################################################
